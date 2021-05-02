ae3a3fb3178b8d210ce916ed792977c1e6e18b49
gianlucaguarini <gianluca.guarini@gmail.com>
2021-05-02 19:53:14 +0200

    added: support typescript code

--- src/utils/ast-nodes-checks.js
+++ src/utils/ast-nodes-checks.js
@@ -7,7 +7,6 @@ const builtinAPIs = Object.keys(globalScope.builtin)
 export const isIdentifier = n => namedTypes.Identifier.check(n)
 export const isLiteral = n => namedTypes.Literal.check(n)
 export const isExpressionStatement = n => namedTypes.ExpressionStatement.check(n)
-export const isObjectExpression = n => namedTypes.ObjectExpression.check(n)
 export const isThisExpression = n => namedTypes.ThisExpression.check(n)
 export const isThisExpressionStatement = n =>
   isExpressionStatement(n) &&
@@ -15,11 +14,8 @@ export const isThisExpressionStatement = n =>
   isThisExpression(n.expression.left.object)
 export const isNewExpression = n => namedTypes.NewExpression.check(n)
 export const isSequenceExpression = n => namedTypes.SequenceExpression.check(n)
-export const isBinaryExpression = n => namedTypes.BinaryExpression.check(n)
-export const isUnaryExpression = n => namedTypes.UnaryExpression.check(n)
 export const isExportDefaultStatement = n => namedTypes.ExportDefaultDeclaration.check(n)
 export const isMemberExpression = n => namedTypes.MemberExpression.check(n)
-export const isArrayExpression = n => namedTypes.ArrayExpression.check(n)
 export const isImportDeclaration = n => namedTypes.ImportDeclaration.check(n)
 
 export const isBrowserAPI = ({name}) => browserAPIs.includes(name)
