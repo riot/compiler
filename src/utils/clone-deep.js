c862155e9151ccae7aeb6da120a5251634ca1ed4
Maksim Kachurin <kachurun@gmail.com>
2021-08-14 13:53:09 +0200

    Fastest version of cloneDeep

--- src/utils/clone-deep.js
+++ src/utils/clone-deep.js
@@ -1,8 +1,24 @@
+/* eslint-disable */
 /**
  * Simple clone deep function, do not use it for classes or recursive objects!
  * @param   {*} source - possibily an object to clone
  * @returns {*} the object we wanted to clone
  */
 export default function cloneDeep(source) {
-  return JSON.parse(JSON.stringify(source))
+  if (typeof source !== 'object' || source == null) {
+    return source
+  }
+
+  const isArray = Array.isArray(source)
+  const clone = isArray ? [...source] : { ...source }
+
+  let key
+
+  for (key in clone) {
+    if (clone.hasOwnProperty(key)) {
+      clone[key] = cloneDeep(clone[key])
+    }
+  }
+
+  return clone
 }
