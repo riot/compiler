ee78e9c5913f86c537948a9468716c83aac0a467
gianlucaguarini <gianluca.guarini@gmail.com>
2023-08-26 22:16:11 +0200

    fixed: prettier errors

--- test/commonjs.spec.cjs
+++ test/commonjs.spec.cjs
@@ -2,9 +2,8 @@ const { expect } = require('chai')
 
 describe('commonjs bundle', () => {
   it('commonjs imports work properly', () => {
-    const {compile} = require('../dist/index.cjs')
+    const { compile } = require('../dist/index.cjs')
 
     expect(() => compile('<test></test>')).to.not.throw()
   })
 })
-
