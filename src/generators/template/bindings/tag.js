b8eb7a2317eb804e1b1f73c78a686754d1769537
Gianluca Guarini <gianluca.guarini@gmail.com>
2025-04-11 23:29:33 +0200

    Revert "fixed: https://github.com/riot/riot/issues/3055"
    This reverts commit d66d3955e833dcba3bf870431d5eb1b718d1b918.

--- src/generators/template/bindings/tag.js
+++ src/generators/template/bindings/tag.js
@@ -24,7 +24,6 @@ import build from '../builder.js'
 import { builders } from '../../../utils/build-types.js'
 import compose from 'cumpa'
 import { simplePropertyNode } from '../../../utils/custom-ast-nodes.js'
-import { isSlotNode } from '../checks.js'
 
 /**
  * Find the slots in the current component and group them under the same id
@@ -65,26 +64,16 @@ function buildSlot(id, sourceNode, sourceFile, sourceCode) {
     ...sourceNode,
     attributes: getNodeAttributes(sourceNode),
   }
+  const [html, bindings] = build(cloneNode, sourceFile, sourceCode)
 
-  // if the first node is a slot then we need to inherit the slots from the parent node (https://github.com/riot/riot/issues/3055)
-  const [html, bindings] = isSlotNode(cloneNode.nodes[0])
-    ? [null, null]
-    : build(cloneNode, sourceFile, sourceCode)
-
-  return builders.objectExpression(
-    [
-      simplePropertyNode(BINDING_ID_KEY, builders.literal(id)),
-      html
-        ? simplePropertyNode(BINDING_HTML_KEY, builders.literal(html))
-        : null,
-      bindings
-        ? simplePropertyNode(
-            BINDING_BINDINGS_KEY,
-            builders.arrayExpression(bindings),
-          )
-        : null,
-    ].filter(Boolean),
-  )
+  return builders.objectExpression([
+    simplePropertyNode(BINDING_ID_KEY, builders.literal(id)),
+    simplePropertyNode(BINDING_HTML_KEY, builders.literal(html)),
+    simplePropertyNode(
+      BINDING_BINDINGS_KEY,
+      builders.arrayExpression(bindings),
+    ),
+  ])
 }
 
 /**
